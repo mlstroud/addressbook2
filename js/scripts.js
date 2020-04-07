d9fc96515e2775980b6e580a6c24b19af2a9833d
Ethan Firpo <efirpo@gmail.com>
2020-04-07 10:51:22 -0700

    add second address button and hide it upon form submission

--- js/scripts.js
+++ js/scripts.js
@@ -37,10 +37,12 @@ AddressBook.prototype.deleteContact = function (id) {
   return false;
 }
 
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, address1, address2) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
+  this.address1 = address1;
+  this.address2 = address2;
 }
 
 Contact.prototype.fullName = function () {
@@ -55,7 +57,7 @@ function displayContactDetails(addressBookToDisplay) {
   var contactsList = $("ul#contacts");
   var htmlForContactInfo = "";
   addressBookToDisplay.contacts.forEach(function (contact) {
-    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + " " + contact.address1 + " " + contact.address2 + "</li>";
   });
   contactsList.html(htmlForContactInfo);
 }
@@ -66,14 +68,33 @@ function showContact(contactId) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
+  $(".address1").html(contact.address1);
+  $(".address2").html(contact.address2);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
 };
 
+// function showSecondAddress() {
+//   $("button#additional-address-button").on("click", "button", function () {
+//     $("#second-address").show();
+//   })
+// };
+
 function attachContactListeners() {
   $("ul#contacts").on("click", "li", function () {
     showContact(this.id);
+  });
+
+  $("button#additional-address-button").on("click", function () {
+    $("#second-address").show();
+  });
+
+
+  $("#buttons").on("click", ".deleteButton", function () {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
   });
 };
 
@@ -84,7 +105,17 @@ $(document).ready(function () {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    var inputtedAddress1 = $("input#new-address1").val();
+    var inputtedAddress2 = $("input#new-address2").val();
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+    $("input#address1").val("");
+    $("input#address2").val("");
+    $("#second-address").hide();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedAddress1, inputtedAddress2);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
   });
