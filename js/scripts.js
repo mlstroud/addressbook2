90e9000ce2c29c8bdf0ec9a38aa091ae1491050d
Ethan Firpo <efirpo@gmail.com>
2020-04-07 09:01:59 -0700

    add spine to HTML and beginning UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,30 @@ function AddressBook() {
   this.currentId = 0;
 }
 
-AddressBook.prototype.addContact = function(contact) {
+AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
   this.contacts.push(contact);
 }
 
-AddressBook.prototype.assignId = function() {
+AddressBook.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 }
 
-AddressBook.prototype.findContact = function(id) {
-  for (i=0; i < this.contacts.length; i++) {
+AddressBook.prototype.findContact = function (id) {
+  for (i = 0; i < this.contacts.length; i++) {
     if (this.contacts[i]) {
       if (this.contacts[i].id == id) {
-        return this.contacts[i];}
-      }  
+        return this.contacts[i];
+      }
+    }
   };
   return false;
 }
 
-AddressBook.prototype.deleteContact = function(id) {
-  for (i=0; i < this.contacts.length; i++) {
-    if (this.contacts[i]) {  
+AddressBook.prototype.deleteContact = function (id) {
+  for (i = 0; i < this.contacts.length; i++) {
+    if (this.contacts[i]) {
       if (this.contacts[i].id == id) {
         delete this.contacts[i];
         return true;
@@ -42,7 +43,12 @@ function Contact(firstName, lastName, phoneNumber) {
   this.phoneNumber = phoneNumber;
 }
 
-Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
 
+
+// User Interface Logic ---------
+$(document).ready(function () {
+  $("form#")
+});
